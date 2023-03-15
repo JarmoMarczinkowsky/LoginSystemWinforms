928aabb05d3f943e5039ef3136d241c98d508265
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-03-15 12:05:56 +0100

    Added new tables (group and usergroup)

---
+++ WindowsFormsApp1/WindowsFormsApp1/Tables/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Tables
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Tables/UserGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Tables
+{
+    public class UserGroup
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
